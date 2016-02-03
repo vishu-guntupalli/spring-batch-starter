7382b60f3bd887a04585e197971a440ab076e380
Vishu Guntupalli <iamno.16@gmail.com>
2016-02-03 16:55:52 -0600

    - Filtering the value of erpConnectionString from .properties file.

--- src/main/java/com/vishu/batch/writer/RestApiWriter.java
+++ src/main/java/com/vishu/batch/writer/RestApiWriter.java
@@ -3,6 +3,7 @@ package com.vishu.batch.writer;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,8 +17,11 @@ public class RestApiWriter {
 	@Autowired
 	private ObjectMapper objectMapper;
 	
+	@Value("${erpConnectString}")
+	private String erpConnectString;
+	
 	public void sendToErp(DiscountProduct discountProduct) throws Exception {
-			restTemplate.postForEntity(new URI("http://localhost:8000/oracle/erp"), objectMapper.writeValueAsString(discountProduct), String.class);
+			restTemplate.postForEntity(new URI(erpConnectString), objectMapper.writeValueAsString(discountProduct), String.class);
 	}
 
 }
